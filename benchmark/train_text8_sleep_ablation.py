e1eecf06e0a7451b9f71c07ee88a9b9ee12c3b2e
jdey4 <jdey4@jhmi.edu>
2026-04-01 00:46:40 -0500

    sleepless run

--- benchmark/train_text8_sleep_ablation.py
+++ benchmark/train_text8_sleep_ablation.py
@@ -68,7 +68,7 @@ class Dataset_converter(Dataset):
         return self.X.shape[0]
 
 #%%
-model_no = 3
+model_no = 2
 # ---- Parameters ----
 total_layers, head_layers, short_term_memory = 5, 2, 4
 
@@ -153,17 +153,23 @@ for _ in range(1):
                 # if model.sleeping:
                 #     print("Sleep on ", model.recon_loss_ema)
 
-        # if ii%20000==0:
-        #     model.sleep_step(total_steps=1025)
+        if ii%20000==0:
+            model.sleep_step(total_steps=1025)
 
 # %%
 # summary = (res_acc, res_bpc)
 # with open('/Users/jd/sleep_experiment/pickle_files/result_text8.pickle', 'wb') as handle:
 #     pickle.dump(summary, handle, protocol=pickle.HIGHEST_PROTOCOL)
-os.makedirs("../saved_models/sleepless_models", exist_ok=True)
+# os.makedirs("../saved_models/sleepless_models", exist_ok=True)
+# torch.save(
+#     model.state_dict(),
+#     f"../saved_models/sleepless_models/model{model_no}_text8.pt"
+# )
+
+os.makedirs("../saved_models/sleep_models", exist_ok=True)
 torch.save(
     model.state_dict(),
-    f"../saved_models/sleepless_models/model{model_no}_text8.pt"
+    f"../saved_models/sleep_models/model{model_no}_text8.pt"
 )
 #%%
 # model = Model(    
